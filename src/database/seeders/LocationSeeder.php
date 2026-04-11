9ef574b5c5ec700eb7206b0c0fb2464637ac86d2
Ahmed Musa <ahmedmusa.swe@gmail.com>
2026-04-11 14:11:29 +0600

    fix: update Shantinagar to Shantinagar Moar in LocationSeeder address

--- src/database/seeders/LocationSeeder.php
+++ src/database/seeders/LocationSeeder.php
@@ -12,7 +12,7 @@ class LocationSeeder extends Seeder
         Location::insert([
             [
 'name' => 'Bailey Road',
-                'address' => 'Bailey Road, Shantinagar, Dhaka 1217',
+                'address' => 'Bailey Road, Shantinagar Moar, Dhaka 1217',
                 'google_maps_url' => 'https://maps.app.goo.gl/UDVbk2jqn4XVSEbVA',
                 'phone' => '01973140768',
                 'is_active' => true,
