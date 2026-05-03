6dd2e165cc73ae912633db0f2617f60a86eba1ca
Ahmed Musa <ahmedmusa.swe@gmail.com>
2026-05-04 02:35:00 +0600

    updated with aws-cli

--- docker/mysql/backup.sh
+++ docker/mysql/backup.sh
@@ -5,8 +5,11 @@ mysqldump -u root -p"${MYSQL_ROOT_PASSWORD}" \
   --all-databases --single-transaction --quick \
   | gzip > "$BACKUP_FILE"
 
-curl -sf -u "${B2_BACKUP_KEY_ID}:${B2_BACKUP_APP_KEY}" \
-  -T "$BACKUP_FILE" \
-  "https://s3.eu-central-003.backblazeb2.com/fdcl-db/fdcl-database-backup/$(basename $BACKUP_FILE)"
+AWS_ACCESS_KEY_ID="${B2_BACKUP_KEY_ID}" \
+AWS_SECRET_ACCESS_KEY="${B2_BACKUP_APP_KEY}" \
+aws s3 cp "$BACKUP_FILE" \
+  "s3://fdcl-db/fdcl-database-backup/$(basename $BACKUP_FILE)" \
+  --endpoint-url https://s3.eu-central-003.backblazeb2.com \
+  --region eu-central-003
 
 rm -f /tmp/fdcl-db-*.sql.gz
